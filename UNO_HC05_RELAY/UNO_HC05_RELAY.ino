3f8d7f6a2433d33fe408acf5ff6610b25fefc236
shennongmin <wupangfee@gmail.com>
2014-12-16 15:01:49 +0800

    Add bt demo

---
+++ UNO_HC05_RELAY/UNO_HC05_RELAY.ino
@@ -0,0 +1,54 @@
+int relay_pin = 2;
+char cmd[10];
+bool valid_cmd = false;
+
+void setup()
+{
+    pinMode(relay_pin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int i;
+    
+    if (Serial.available() > 0)
+    { 
+        for (i = 0; i < 10; i++)
+        {
+            cmd[i] = '\0';
+        }   
+        
+        for (i = 0; i < 9; i++)
+        {   
+            if (Serial.available() > 0)
+            {
+                cmd[i] = Serial.read();
+                Serial.write(cmd[i]);
+                delay(1);
+            }
+            else
+            {
+                break;
+            }
+        }
+        valid_cmd = true;
+    }
+    
+    if (valid_cmd)
+    {
+        valid_cmd = false;
+        if (0 == strncmp(cmd, "on", 2))
+        {
+            digitalWrite(relay_pin, HIGH);
+        }
+        else if (0 == strncmp(cmd, "off", 3))
+        {
+            digitalWrite(relay_pin, LOW);
+        }
+        else
+        { 
+        }
+    }
+    delay(10);
+}
